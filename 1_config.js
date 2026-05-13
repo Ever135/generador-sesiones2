7ebbaeddbe6e605d2625907c6c494071336317ed
Ever135 <ebautistah.22@ispa.edu.pe>
2026-05-12 21:22:57 -0500

    Add configuration file with color constants

---
+++ 1_config.js
@@ -0,0 +1,17 @@
+// 1_config.js
+
+const MAIN = "1B3A6B";
+const ACC = "C0392B";
+const GREEN = "1A6B1A";
+const PURP = "6A1B9A";
+const GOLD = "7A5200";
+const TEAL = "0E6655";
+
+module.exports = {
+  MAIN,
+  ACC,
+  GREEN,
+  PURP,
+  GOLD,
+  TEAL
+};
